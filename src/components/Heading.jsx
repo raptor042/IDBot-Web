d37bffcf5731392445611f9ac807325a24a1599c
Benji Chimex <benjichimex@Benjis-MacBook-Pro.local>
2023-12-09 20:15:57 +0100

    another commit

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,7 +3,7 @@ export default function Heading() {
         <div id="heading" className="absolute top-40 left-20">
             <h1 className="text-2xl font-bold my-2" style={{ color : "#101530" }}>Welcome to the IDBot Protocol</h1>
             <span className="text-lg text-gray-500 my-2">
-                Lets's set-up and configure your IDBot identity
+                Lets set-up and configure your IDBot identity
             </span>
         </div>
     )
