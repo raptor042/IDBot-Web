72ad30a5260a0b9f7d80d5b3b3278095f901f9ac
Benji Chimex <benjichimex@Benjis-MacBook-Pro.local>
2023-12-12 13:49:55 +0100

    another commit

--- src/components/CreateDID.jsx
+++ src/components/CreateDID.jsx
@@ -7,7 +7,7 @@ export default function CreateDID() {
 
     return (
         <div id="create-did" className="">
-            <button onClick={() => window.location.assign("http://localhost:3000/setup")} className="p-4 text-bold text-lg text-white flex rounded-lg bg-black hover:bg-amber-400">
+            <button onClick={() => window.location.assign("https://id-bot-benjamin1234-ben.vercel.app/setup")} className="p-4 text-bold text-lg text-white flex rounded-lg bg-black">
                 Begin Set-up
             </button>
         </div>
